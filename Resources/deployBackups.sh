80bdf68059d538c4c278877a81f9fff949ed7eb4
Cybera Sysadmin <sysadmin@cybera.ca>
2016-11-18 11:54:32 -0700

    backups enabled

---
+++ Resources/deployBackups.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "export OS_TENANT_NAME=%TENANT%
+export OS_USERNAME=%USERID%
+export OS_PASSWORD=%PASSWORD%
+export OS_AUTH_URL="https://dair-hnl-v02.dair-atir.canarie.ca:5000/v2.0/"
+export OS_AUTH_STRATEGY=keystone
+export OS_REGION_NAME=honolulu" >> /home/ubuntu/openrc
+
+sudo apt-get install -y python-pip
+sudo apt-get -y install python-swiftclient
+
+sudo mkdir /var/lib/postgres_backups
+sudo bash -c "cat > /usr/local/bin/backup_postgres.sh <<EOF
+#!/bin/bash
+backup_dir="/var/lib/postgres_backups"
+filename="postgres-`hostname`-`eval date +%Y%m%d`.sql.gz"
+fullpath="${backup_dir}/${filename}"
+
+# Dump the entire  database
+pg_dumpall -U postgres -h localhost | gzip > $fullpath
+
+if [[ $? != 0 ]]; then
+    echo "Error dumping database"
+      exit 1
+    fi
+
+    # Delete backups older than 20 days
+    find $backup_dir -ctime +20 -type f -delete
+
+    # Upload to swift
+    source /home/ubuntu/openrc
+    cd $backup_dir
+    swift upload postgresql $filename > /dev/null
+    if [[ $? != 0 ]]; then
+        echo "Error uploading backup"
+          exit 1
+        fi
+EOF"
+sudo chmod +x  /usr/local/bin/backup_postgres.sh
+sudo crontab -l > mycron 2>/dev/null
+echo "30 03 */3 * * /usr/local/bin/backup_postgres.sh > /dev/null" >> mycron
+echo "30 03 01 */3 * /usr/local/bin/backup_postgres.sh > /dev/null" >> mycron
+sudo crontab mycron
+rm mycron
--- Resources/deployPostgreSQL.sh
+++ Resources/deployPostgreSQL.sh
@@ -8,14 +8,18 @@ sudo mount $vol /opt/postgresql_data
 echo "$vol /opt/postgresql_data ext3 defaults 0 1 "| sudo tee --append  /etc/fstab
 
 sudo apt-get -y -q install postgresql postgresql-contrib
+location1=$(echo $(sudo -u postgres psql -c "show config_file";) | awk '{print $3}')
+location2=$(echo $(sudo -u postgres psql -c "show hba_file";) | awk '{print $3}')
 sudo service postgresql stop
 
 sudo rsync -av /var/lib/postgresql  /opt/postgresql_data
 sudo rm -r /var/lib/postgresql/
 sudo ln -s /opt/postgresql_data/postgresql /var/lib/postgresql
 
-echo "host    all             all             %CONNECTION_IP%/32            md5"| sudo tee --append /etc/postgresql/9.3/main/pg_hba.conf
-sudo sed -i -e "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.3/main/postgresql.conf
+echo "host    all             all             %CONNECTION_IP%/32            md5"| sudo tee --append $location2
+sudo sed -i -e "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" $location1
 
 sudo service postgresql start
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD '%POSTGRES_PASSWORD%';"
+echo "*:*:*:postgres:%POSTGRES_PASSWORD%" | sudo tee --append /root/.pgpass
+sudo chmod 600 /root/.pgpass
